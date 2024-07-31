6908022381d6f3c251b5db538d9a638481fccff8
anis safia <anissafia110@gmail.com>
2024-07-31 13:15:35 +0100

    FIX Task 4

--- 0x03-React_component/task_3/dashboard/src/BodySection/BodySectionWithMarginBottom.js
+++ 0x03-React_component/task_3/dashboard/src/BodySection/BodySectionWithMarginBottom.js
@@ -1,7 +1,7 @@
 import React from "react";
 import BodySection from "./BodySection";
 import PropTypes from "prop-types";
-import "./BodySectionWithMarginBottom.css";
+import "./BodySection.css";
 
 const BodySectionWithMarginBottom = ({ title, children }) => {
   return (
