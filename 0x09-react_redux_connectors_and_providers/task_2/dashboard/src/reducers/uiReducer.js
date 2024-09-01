276403cb168086dbbaf9459269ccc6b4fa705798
anis safia <anissafia110@gmail.com>
2024-09-01 21:22:01 +0100

    feat: Modify the uiReducer

--- 0x09-react_redux_connectors_and_providers/task_2/dashboard/src/reducers/uiReducer.js
+++ 0x09-react_redux_connectors_and_providers/task_2/dashboard/src/reducers/uiReducer.js
@@ -1,31 +1,45 @@
+import { Map } from "immutable";
+import { logout } from "../actions/uiActionCreators";
+
 import {
+  LOGIN,
+  LOGOUT,
   DISPLAY_NOTIFICATION_DRAWER,
   HIDE_NOTIFICATION_DRAWER,
   LOGIN_SUCCESS,
   LOGIN_FAILURE,
-  LOGOUT,
 } from "../actions/uiActionTypes";
-import { Map } from "immutable";
 
-export const defaultState = Map({
+export const initialState = {
   isNotificationDrawerVisible: false,
   isUserLoggedIn: false,
-  user: {},
-});
+  user: null,
+};
 
-export default function uiReducer(state = defaultState, action) {
+const uiReducer = (state = Map(initialState), action) => {
   switch (action.type) {
     case DISPLAY_NOTIFICATION_DRAWER:
-      return defaultState.set("isNotificationDrawerVisible", true);
+      return state.set("isNotificationDrawerVisible", true);
+
     case HIDE_NOTIFICATION_DRAWER:
-      return defaultState.set("isNotificationDrawerVisible", false);
+      return state.set("isNotificationDrawerVisible", false);
+
     case LOGIN_SUCCESS:
-      return defaultState.set("isUserLoggedIn", true);
+      return state.set("isUserLoggedIn", true);
+
     case LOGIN_FAILURE:
-      return defaultState.set("isUserLoggedIn", false);
+      return state.set("isUserLoggedIn", false);
+
+    case LOGIN:
+      return state.set("user", action.user);
+
     case LOGOUT:
-      return defaultState.set("isUserLoggedIn", false);
+      return state.set("isUserLoggedIn", false).set("user", null);
+
     default:
-      return state;
+      break;
   }
-}
+  return state;
+};
+
+export default uiReducer;
